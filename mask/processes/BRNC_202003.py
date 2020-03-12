7730c71e0bfbf2044e5be767d8d745751bff2abc
Johannes Wüthrich <johannes@deragent.net>
2020-03-12 17:59:13 +0100

    Adds mask label layer

--- mask/processes/BRNC_202003.py
+++ mask/processes/BRNC_202003.py
@@ -1,5 +1,6 @@
 from .. import config
 
+
 config.GLOBAL["LAYERS"]["WAFER_OUTLINE"] = (52, 1)
 
 config.GLOBAL["LAYERS"]["MASK_OUTLINE"] = (61, 2)
@@ -7,3 +8,6 @@ config.GLOBAL["LAYERS"]["MASK_OUTLINE"] = (61, 2)
 config.GLOBAL["LAYERS"]["ALIGNMENT_MARKS"] = (11, 0)
 config.GLOBAL["LAYERS"]["CONTACT_DOPING"] = (12, 0)
 config.GLOBAL["LAYERS"]["METALIZATION"] = (13, 0)
+
+
+config.GLOBAL["LAYERS"]["MASK_LABEL"] =(62, 3)
