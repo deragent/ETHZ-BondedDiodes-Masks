7f2ec4dbb2c04e081933241f693868b41940a635
Johannes Wüthrich <johannes@deragent.net>
2020-03-12 15:50:56 +0100

    Adds base element

---
+++ mask/elements/Element.py
@@ -0,0 +1,36 @@
+from .. import config
+
+class Element():
+
+    def __init__(self, parent, name, layers=None, lib=None):
+
+        self.parent = parent
+        self.name = name
+
+        if layers is None:
+            layers = config.GLOBAL["LAYERS"]
+        self.layers = layers
+
+        if lib is None:
+            lib = config.GLOBAL["LIB"]
+        self.lib = lib
+
+        self.cell = self.lib.new_cell(name)
+
+        self.construct()
+
+        self._finalize()
+
+    def construct(self):
+        raise NotImplementedError()
+
+    def _finalize(self):
+
+        bbox = self.cell.get_bounding_box()
+
+        self.width = bbox[1,0] - bbox[0,0]
+        self.height = bbox[1,1] - bbox[0,1]
+
+        if self.parent is not None:
+            print(self.parent.name)
+            self.parent.add(self.cell)
