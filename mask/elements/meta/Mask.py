5d1a1f743808087fb75ac6668d1ab4c67d345536
Johannes Wüthrich <johannes@deragent.net>
2020-03-12 17:59:24 +0100

    Adds ability to mask for label text

--- mask/elements/meta/Mask.py
+++ mask/elements/meta/Mask.py
@@ -4,15 +4,22 @@ from .. import Element
 
 class Mask(Element):
 
-    def __init__(self, parent, name, size, margin=4000, layers=None, lib=None):
+    def __init__(self, parent, name, size, text, margin=4000, layers=None, lib=None):
 
         self.size = size
         self.margin = margin
+        self.text = text
 
         super().__init__(parent, name, layers, lib)
 
     def construct(self):
 
+        self._constructFrame()
+
+        if len(self.text) > 0:
+            self._constructLabel()
+
+    def _constructFrame(self):
         s2 = self.size/2
         s2m = self.size/2 - self.margin
 
@@ -37,3 +44,49 @@ class Mask(Element):
             outline = gdspy.boolean(outline, hole, 'not', **self.layers["MASK_OUTLINE"])
 
         self.cell.add(outline)
+
+
+    def _constructLabel(self):
+        T_HEIGHT = 2000
+
+        x0 = -self.size/2 + 3*self.margin
+        y0 = -self.size/2 + 3*self.margin
+
+        text = self.lib.new_cell(self.name + '_LABEL')
+
+        maxlabel = 0
+        y = y0
+
+        for label, value in self.text.items():
+            t = gdspy.Text(label, T_HEIGHT, position=(x0, y), **self.layers["MASK_LABEL"])
+            text.add(t)
+
+            bbox = t.get_bounding_box()
+            length = bbox[1,0] - bbox[0,0]
+            if length > maxlabel: maxlabel = length
+
+            y += T_HEIGHT*1.3
+
+        maxvalue = 0
+        y = y0
+
+        for label, value in self.text.items():
+            t = gdspy.Text(value, T_HEIGHT, position=(x0 + maxlabel*1.3, y), **self.layers["MASK_LABEL"])
+            text.add(t)
+
+            bbox = t.get_bounding_box()
+            length = bbox[1,0] - bbox[0,0]
+            if length > maxvalue: maxvalue = length
+
+            y += T_HEIGHT*1.3
+
+        rect = gdspy.Rectangle(
+            (x0 - T_HEIGHT, y0 - T_HEIGHT),
+            (x0 + maxlabel*1.3 + maxvalue + T_HEIGHT, y0 + len(self.text)*T_HEIGHT*1.3 + T_HEIGHT),
+            **self.layers["MASK_LABEL"]
+        )
+        outset = gdspy.offset(rect, T_HEIGHT/2)
+
+        text.add(gdspy.boolean(outset, rect, 'not', **self.layers["MASK_LABEL"]))
+
+        self.cell.add(text)
