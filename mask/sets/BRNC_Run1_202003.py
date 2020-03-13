d827b70117a45b3e30750ec868454f0b52491aac
Johannes Wüthrich <johannes@deragent.net>
2020-03-13 15:22:36 +0100

    Add 6 and 4mm diodes instead of 5mm

--- mask/sets/BRNC_Run1_202003.py
+++ mask/sets/BRNC_Run1_202003.py
@@ -15,6 +15,8 @@ config.GLOBAL["LIB"] = lib
 
 top = lib.new_cell('TOP_CELL')
 
+### Create meta cells
+
 w = Wafer(top, 'WAFER', 150000)
 m = Mask(top, 'MASK', 7*25400, {
     "DATE": "2020-03-12",
@@ -22,26 +24,30 @@ m = Mask(top, 'MASK', 7*25400, {
     "LAYOUT": "DIODE-RUN-1"
 })
 
+
+### Create diode cells
+
 d15 = Diode(None, 'DIODE_15mm', 15000)
 d12 = Diode(None, 'DIODE_12mm', 12000)
 d8 = Diode(None, 'DIODE_8mm', 8000)
-d5 = Diode(None, 'DIODE_5mm', 5000)
+d6 = Diode(None, 'DIODE_6mm', 6000)
+d4 = Diode(None, 'DIODE_4mm', 4000)
 d2 = Diode(None, 'DIODE_2mm', 2000)
 
 spacing = 1000
 
 offset = spacing/2
 
-ystart = [-52000, -53500, -50000, -45000, -41000]
+ystart = [-53500, -55000, -50500, -45000, -38500, -33500]
 
-for ii, d in enumerate([d15, d12, d8, d5, d2]):
+for ii, d in enumerate([d15, d12, d8, d6, d4, d2]):
 
     yspacing = d.height + spacing
 
     xstart = offset + d.width/2
 
     n = math.ceil(-2*ystart[ii] / yspacing)
-    if ii <= 1:
+    if ii <= 2:
         n -= 1
 
     top.add(gdspy.CellArray(d.cell, 1, n, (0, yspacing), origin=(xstart, ystart[ii])))
@@ -49,6 +55,10 @@ for ii, d in enumerate([d15, d12, d8, d5, d2]):
 
     offset += d.width + spacing
 
+
+
+
+### Save the gds file
 
 lib.write_gds('BRNC_Run1_202003.gds')
 
