2880b79e90637cfa40e3d9ec02a2bd82b2b92b1e
Johannes Wüthrich <johannes@deragent.net>
2020-03-13 14:46:59 +0100

    Corrects form and spacing of the guard rings

--- mask/elements/electrical/Diode.py
+++ mask/elements/electrical/Diode.py
@@ -20,8 +20,10 @@ class Diode(Element):
         super().__init__(parent, name, layers, lib)
 
     def construct(self):
-        self.__createDiode(self.layers["CONTACT_DOPING"], overhang = self.overhang)
-        self.__createDiode(self.layers["METALIZATION"], overhang = 0)
+        self.__createDiode(self.layers["CONTACT_DOPING"], overhang = 0)
+        self.__createDiode(self.layers["METALIZATION"], overhang = -self.overhang)
+        self.__createGuardRings(self.layers["CONTACT_DOPING"], overhang = 0)
+        self.__createGuardRings(self.layers["METALIZATION"], overhang = -self.overhang)
 
     def __createDiode(self, layer, overhang = 0):
 
@@ -31,13 +33,21 @@ class Diode(Element):
 
         rect = gdspy.Rectangle((-sA, -sB), (sA, sB), **layer)
 
+        self.cell.add(rect)
+
+    def __createGuardRings(self, layer, overhang = 0):
+
+        sA = self.A/2
+        sB = self.B/2
+        oh = overhang
+
+        rect = gdspy.Rectangle((-sA, -sB), (sA, sB), **layer)
+
         GuardRings(layer, self.cell, rect, [
-            (50-2*oh, 100+2*oh),
+            (50-oh, 50+2*oh),
             (50-2*oh, 50+2*oh),
             (50-2*oh, 50+2*oh),
             (50-2*oh, 50+2*oh),
             (50-2*oh, 50+2*oh),
             (50-2*oh, 100+2*oh)
         ])
-
-        self.cell.add(rect)
