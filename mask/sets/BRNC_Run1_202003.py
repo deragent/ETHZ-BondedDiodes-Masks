83afaddb7382a0877f88d9f5dd62a1934885e0f6
Johannes Wüthrich <johannes@deragent.net>
2020-03-26 20:49:40 +0100

    Add test resistances to layout

--- mask/sets/BRNC_Run1_202003.py
+++ mask/sets/BRNC_Run1_202003.py
@@ -9,6 +9,7 @@ from mask.processes import BRNC_202003
 from mask.elements.electrical import Diode
 from mask.elements.meta import Wafer
 from mask.elements.meta import Mask
+from mask.elements.test import Resistance
 
 lib = gdspy.GdsLibrary()
 config.GLOBAL["LIB"] = lib
@@ -56,6 +57,34 @@ for ii, d in enumerate([d15, d12, d8, d6, d4, d2]):
     offset += d.width + spacing
 
 
+### Add test structures
+testres = lib.new_cell('TEST_TOP')
+
+r5 = Resistance(None, 'RES_5mm', 5000)
+r10 = Resistance(None, 'RES_10mm', 10000)
+r15 = Resistance(None, 'RES_15mm', 15000)
+r20 = Resistance(None, 'RES_20mm', 20000)
+
+
+testres.add(gdspy.CellReference(r15.cell, origin=(-1.1*r15.width, 0)))
+testres.add(gdspy.CellReference(r15.cell, origin=(0*r15.width, 0)))
+testres.add(gdspy.CellReference(r15.cell, origin=(+1.1*r15.width, 0)))
+
+testres.add(gdspy.CellReference(r20.cell, origin=(-0.55*r20.width, 1.5*r15.height)))
+testres.add(gdspy.CellReference(r20.cell, origin=(0.55*r20.width, 1.5*r15.height)))
+
+testres.add(gdspy.CellReference(r10.cell, origin=(-1.1*r10.width, 3*r15.height)))
+testres.add(gdspy.CellReference(r10.cell, origin=(0*r10.width, 3*r15.height)))
+testres.add(gdspy.CellReference(r10.cell, origin=(+1.1*r10.width, 3*r15.height)))
+
+testres.add(gdspy.CellReference(r5.cell, origin=(-1.1*r5.width, 4.5*r15.height)))
+testres.add(gdspy.CellReference(r5.cell, origin=(0*r5.width, 4.5*r15.height)))
+testres.add(gdspy.CellReference(r5.cell, origin=(+1.1*r5.width, 4.5*r15.height)))
+
+
+
+top.add(gdspy.CellReference(testres, rotation=90, origin=(-62000, 0)))
+top.add(gdspy.CellReference(testres, rotation=-90, origin=(+62000, 0)))
 
 
 ### Save the gds file
