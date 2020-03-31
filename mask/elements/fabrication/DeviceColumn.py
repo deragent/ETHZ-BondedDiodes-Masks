d6ac6cb574549263609093173d43519f78f7d76f
Johannes Wüthrich <johannes@deragent.net>
2020-03-31 20:06:52 +0200

    Adds dicing lines to device column

--- mask/elements/fabrication/DeviceColumn.py
+++ mask/elements/fabrication/DeviceColumn.py
@@ -2,6 +2,8 @@ import gdspy
 
 from .. import Element
 
+from ...forms import DicingLine
+
 class DeviceColumn(Element):
 
     def __init__(self, parent, name, device, x, ymin, ymax, margin=150, dicingwidth=100, keepout=[], layers=None, lib=None):
@@ -21,13 +23,16 @@ class DeviceColumn(Element):
     def construct(self):
 
         m = self.margin
-        dw = self.dicingwidth
 
         bbox = self.device.get_bounding_box()
         devw = bbox[1,0] - bbox[0,0]
         devh = bbox[1,1] - bbox[0,1]
 
+        doffset = devw / 2 + m
+
         origin = (self.x, self.ymin + m + devh/2)
+
+        ymax = 0
 
         while True:
 
@@ -41,9 +46,27 @@ class DeviceColumn(Element):
                 origin = (origin[0], origin[1] + collision + 3*m)
                 continue
 
+            ref_ymin = ref.get_bounding_box()[0,1]
+            ref_ymax = ref.get_bounding_box()[1,1]
             self.cell.add(ref)
 
+            ## Add top and bottom dicing line
+            dicing_bottom = DicingLine(self.layers["DICING"], self.cell,
+                self.dicingwidth, (self.x - doffset, ref_ymin - m), (self.x + doffset, ref_ymin - m))
+            dicing_top = DicingLine(self.layers["DICING"], self.cell,
+                self.dicingwidth, (self.x - doffset, ref_ymax + m), (self.x + doffset, ref_ymax + m))
+
+            ## TODO fix proper detection for top and bottom end of vertical dicing lines
+            ymax = ref_ymax
+
+
             origin = (origin[0], origin[1] + devh + 2*m)
+
+        ## Add left and right dicing line
+        dicing_left = DicingLine(self.layers["DICING"], self.cell,
+            self.dicingwidth, (self.x - doffset, self.ymin), (self.x - doffset, ymax + m))
+        dicing_right = DicingLine(self.layers["DICING"], self.cell,
+            self.dicingwidth, (self.x + doffset, self.ymin), (self.x + doffset, ymax + m))
 
 
     def __inKeepout(self, bbox):
