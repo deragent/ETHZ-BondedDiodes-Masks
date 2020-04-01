7843cf9ac436ce58dd1969dbaba1715d151f027e
Johannes Wüthrich <johannes@deragent.net>
2020-04-01 16:35:15 +0200

    Adds passivation opening to Metal VDP

--- mask/elements/test/VanDerPauw.py
+++ mask/elements/test/VanDerPauw.py
@@ -1,4 +1,5 @@
 import gdspy
+import math
 
 from ...forms import VanDerPauwClover
 
@@ -6,10 +7,20 @@ from .. import Element
 
 class VanDerPauwMetal(Element):
 
-    def __init__(self, parent, name, radius, fraction=6, layers=None, lib=None):
+    def __init__(
+        self, parent, name,
+        radius=None, fraction=6, contactw=1000, contactspacing=2540,
+        layers=None, lib=None
+    ):
+
+        if radius is None:
+            radius = 0.5*math.sqrt(2)*(contactspacing + contactw)
 
         self.radius = radius
         self.fraction = fraction
+
+        self.contactspacing = contactspacing
+        self.contactw = contactw
 
         super().__init__(parent, name, layers, lib)
 
@@ -19,6 +30,18 @@ class VanDerPauwMetal(Element):
             self.layers["METALIZATION"], self.cell,
             self.radius, self.fraction
         )
+
+        w = self.contactw
+        for h in [-1, +1]:
+            x = h * self.contactspacing/2
+            for v in [-1, +1]:
+                y = v * self.contactspacing/2
+
+                self.cell.add(gdspy.Rectangle(
+                    (x - w/2, y - w/2), (x + w/2, y + w/2),
+                    **self.layers["PASSIVATION_OPEN"]
+                ))
+
 
 
 class VanDerPauwContact(Element):
