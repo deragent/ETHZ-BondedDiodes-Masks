6a6569e319c5b3d535cc81c5ed3ee4ef449c0726
Johannes Wüthrich <johannes@deragent.net>
2020-05-13 18:35:43 +0200

    Adds macro for Run1 test structure set

---
+++ mask/macros/Run1_TestStructures.py
@@ -0,0 +1,33 @@
+import gdspy
+
+from mask.elements.test import VanDerPauwMetal, VanDerPauwContact, TLMContact
+
+CONTACT_WIDTH = 1000
+CONTACT_SPACING = 2540
+OVERHANG = 10
+
+def Run1_TestSet(lib):
+
+    testset = lib.new_cell('TEST_SET')
+
+    tlm = TLMContact(
+        None, 'TLM',
+        [1000, 2000, 3000, 5000, 8000, 10000, 12000, 15000],
+        contactw=CONTACT_WIDTH, contactspacing=CONTACT_SPACING, overhang=OVERHANG
+    )
+
+    vdp_metal = VanDerPauwMetal(
+        None, 'VDPMetal',
+        contactw=CONTACT_WIDTH, contactspacing=CONTACT_SPACING
+    )
+    vdp_contact = VanDerPauwContact(
+        None, 'VDPContact',
+        contactw=CONTACT_WIDTH, contactspacing=CONTACT_SPACING, overhang=OVERHANG
+    )
+
+    testset.add(gdspy.CellReference(vdp_metal.cell, origin=(0, 0)))
+    testset.add(gdspy.CellReference(vdp_contact.cell, origin=(2*CONTACT_SPACING, 0)))
+    testset.add(gdspy.CellReference(tlm.cell,
+        origin=(3.5*CONTACT_SPACING - 0.5*CONTACT_WIDTH - OVERHANG, 0)))
+
+    return testset
--- mask/macros/__init__.py
+++ mask/macros/__init__.py
@@ -1,1 +1,2 @@
 from .Run1_TestRes import Run1_TestRes
+from .Run1_TestStructures import Run1_TestSet
