7a4e9d59f83426cacd140df6074621c4e5e7f2cc
Johannes Wüthrich <johannes@deragent.net>
2020-09-09 15:27:39 +0200

    Adds optional window to diode element

--- mask/elements/electrical/Diode.py
+++ mask/elements/electrical/Diode.py
@@ -6,7 +6,7 @@ from ...forms import GuardRings
 
 class Diode(Element):
 
-    def __init__(self, parent, name, size, rounding=0, overhang=10, layers=None, lib=None, tolerance=40):
+    def __init__(self, parent, name, size, rounding=0, window=0, overhang=10, layers=None, lib=None, tolerance=40):
 
         self.tolerance = 40
 
@@ -18,20 +18,19 @@ class Diode(Element):
             self.B = size
 
         self.overhang = overhang
-        if rounding > 0:
-            self.rounding = rounding
-        else:
-            self.rounding = None
+
+        self.rounding = rounding if rounding > 0 else None
+        self.window = window if window > 0 else None
 
         super().__init__(parent, name, layers, lib)
 
     def construct(self):
         self.__createDiode(self.layers["CONTACT_DOPING"], overhang = 0)
-        self.__createDiode(self.layers["METALIZATION"], overhang = -self.overhang)
+        self.__createDiode(self.layers["METALIZATION"], overhang = -self.overhang, window = self.window)
         self.__createGuardRings(self.layers["CONTACT_DOPING"], overhang = 0)
         self.__createGuardRings(self.layers["METALIZATION"], overhang = -self.overhang)
 
-    def __createDiode(self, layer, overhang = 0):
+    def __createDiode(self, layer, overhang = 0, window=None):
 
         r = self.rounding if self.rounding is not None else 0
 
@@ -45,6 +44,10 @@ class Diode(Element):
             form = rect
         else:
             form = gdspy.offset(rect, r, join='round', tolerance=self.tolerance, **layer)
+
+        if window is not None:
+            hole = gdspy.Round((0,0), window/2, tolerance=self.tolerance)
+            form = gdspy.boolean(form, hole, 'not', **layer)
 
         self.cell.add(form)
 
