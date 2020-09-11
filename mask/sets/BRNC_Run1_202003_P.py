4c9e29617578257b5bc2f244acf0d0a9239cce87
Johannes Wüthrich <johannes@deragent.net>
2020-09-11 17:37:59 +0200

    Adds specific N and P side projects

---
+++ mask/sets/BRNC_Run1_202003_P.py
@@ -0,0 +1,18 @@
+import argparse
+import os
+
+from .BRNC_Run1_202003 import main
+
+parser = argparse.ArgumentParser(description='Create the mask layout for the BRNC Run-1 [202003] - P-Side.')
+parser.add_argument('-o', '--output', required=True,
+                    help='Name of the output file.')
+parser.add_argument('-e', '--export', default=None,
+                    help='Flag to export the fabrication mask layers into individual files.')
+
+args = parser.parse_args()
+
+# Add the additional side argument
+args.side = "P"
+
+# Call the BRNC_202003 main function
+main(args)
