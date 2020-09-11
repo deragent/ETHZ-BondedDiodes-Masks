2051421e3349e974ef2e4a64924e1e3d8f1fde41
Johannes Wüthrich <johannes@deragent.net>
2020-09-11 17:07:57 +0200

    Adds the possibility to add a label on the metal

--- mask/elements/electrical/Diode.py
+++ mask/elements/electrical/Diode.py
@@ -6,7 +6,7 @@ from ...forms import GuardRings
 
 class Diode(Element):
 
-    def __init__(self, parent, name, size, rounding=0, window=0, overhang=10, layers=None, lib=None, tolerance=40):
+    def __init__(self, parent, name, size, rounding=0, window=0, overhang=10, label=None, layers=None, lib=None, tolerance=40):
 
         self.tolerance = 40
 
@@ -22,15 +22,17 @@ class Diode(Element):
         self.rounding = rounding if rounding > 0 else None
         self.window = window if window > 0 else None
 
+        self.label = label
+
         super().__init__(parent, name, layers, lib)
 
     def construct(self):
         self.__createDiode(self.layers["CONTACT_DOPING"], overhang = 0)
-        self.__createDiode(self.layers["METALIZATION"], overhang = -self.overhang, window = self.window)
+        self.__createDiode(self.layers["METALIZATION"], overhang = -self.overhang, window = self.window, label = self.label)
         self.__createGuardRings(self.layers["CONTACT_DOPING"], overhang = 0)
         self.__createGuardRings(self.layers["METALIZATION"], overhang = -self.overhang)
 
-    def __createDiode(self, layer, overhang = 0, window=None):
+    def __createDiode(self, layer, overhang = 0, window=None, label=None):
 
         r = self.rounding if self.rounding is not None else 0
 
@@ -48,6 +50,11 @@ class Diode(Element):
         if window is not None:
             hole = gdspy.Round((0,0), window/2, tolerance=self.tolerance)
             form = gdspy.boolean(form, hole, 'not', **layer)
+
+        if label is not None:
+            t = gdspy.Text(label[0], label[1], position=(self.A*0.45 - label[1], self.B*0.45 - label[1]), **layer)
+            form = gdspy.boolean(form, t, 'not', **layer)
+
 
         self.cell.add(form)
 
