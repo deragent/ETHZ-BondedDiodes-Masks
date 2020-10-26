a22da0071db23b1c08d55816c1d3adf0af5f4edc
Johannes Wüthrich <johannes@deragent.net>
2020-10-26 14:44:04 +0100

    Wafer Element now uses the Ring Form

--- mask/elements/meta/Wafer.py
+++ mask/elements/meta/Wafer.py
@@ -3,6 +3,7 @@ import math
 import gdspy
 
 from .. import Element
+from ...forms import Ring
 
 class Wafer(Element):
 
@@ -18,22 +19,8 @@ class Wafer(Element):
     def construct(self):
 
         r = self.size/2
-        f2 = self.flat/2
 
-        bottom = math.sqrt(r**2 - f2**2)
-
-        round = gdspy.Round((0, 0), r, tolerance=10)
-        flat = gdspy.Rectangle((-r, -r*1.1), (r, -bottom))
-
-        outline = gdspy.boolean(round, flat, 'not', **self.layers["WAFER_OUTLINE"])
-
-        margin = gdspy.offset(outline, -self.margin, tolerance=100)
-
-        wafer = gdspy.boolean(outline, margin, 'not', **self.layers["WAFER_OUTLINE"])
-
-        # offset1 = gdspy.offset(self.base, offset + spec[0], join='round', tolerance=self.tolerance)
-        # offset2 = gdspy.offset(self.base, offset + spec[0] + spec[1], join='round', tolerance=self.tolerance)
-        #
-        # diff = gdspy.boolean(offset2, offset1, 'not', **self.layers["WAFER_OUTLINE"])
-
-        self.cell.add(wafer)
+        ring = Ring(
+            self.layers["WAFER_OUTLINE"], self.cell,
+            r-self.margin, r, flat=self.flat
+        )
