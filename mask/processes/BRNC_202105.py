487356990e2f20335df6e5f193a55759cc281fe0
Johannes Wüthrich <johannes@deragent.net>
2021-06-15 14:53:11 +0200

    Adds another layer for isolation trench etching
    - Is an alias for the alignment mark etching for now.

--- mask/processes/BRNC_202105.py
+++ mask/processes/BRNC_202105.py
@@ -18,7 +18,9 @@ config.GLOBAL["LAYERS"]["DICING"] = (53, 4)
 config.GLOBAL["LAYERS"]["MASK_CORNERS"] = (60, 2)
 config.GLOBAL["LAYERS"]["MASK_OUTLINE"] = (61, 2)
 
+# Only one etching step for alignment and isolation trench
 config.GLOBAL["LAYERS"]["ALIGNMENT_MARKS"] = (11, 0)
+config.GLOBAL["LAYERS"]["ISOLATION_TRENCH"] = (11, 0)
 config.GLOBAL["LAYERS"]["CONTACT_DOPING"] = (12, 0)
 config.GLOBAL["LAYERS"]["METALIZATION"] = (13, 0)
 config.GLOBAL["LAYERS"]["PASSIVATION_OPEN"] = (14, 0)
