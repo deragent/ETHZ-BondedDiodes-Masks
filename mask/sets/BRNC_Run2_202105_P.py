1dc14c4fe8942e8a9f37b6e46f1a20613aeb9d51
Johannes Wüthrich <johannes@deragent.net>
2021-06-17 01:50:35 +0200

    Defines alias mask sets for N and P side

---
+++ mask/sets/BRNC_Run2_202105_P.py
@@ -0,0 +1,18 @@
+import argparse
+import os
+
+from .BRNC_Run2_202105 import main
+
+parser = argparse.ArgumentParser(description='Create the mask layout for the BRNC Run-2 [202105] - P-Side.')
+parser.add_argument('-o', '--output', required=True,
+                    help='Name of the output file.')
+parser.add_argument('-e', '--export', default=None,
+                    help='Flag to export the fabrication mask layers into individual files.')
+
+args = parser.parse_args()
+
+# Add the additional side argument
+args.side = "P"
+
+# Call the BRNC_202105 main function
+main(args)
