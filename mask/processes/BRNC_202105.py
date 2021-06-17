d8970e4f17f7ef01b8ac118544319923b54cced5
Johannes Wüthrich <johannes@deragent.net>
2021-06-17 18:44:27 +0200

    Adds own layer for isolation trench etching

--- mask/processes/BRNC_202105.py
+++ mask/processes/BRNC_202105.py
@@ -20,10 +20,10 @@ config.GLOBAL["LAYERS"]["MASK_OUTLINE"] = (61, 2)
 
 # Only one etching step for alignment and isolation trench
 config.GLOBAL["LAYERS"]["ALIGNMENT_MARKS"] = (11, 0)
-config.GLOBAL["LAYERS"]["ISOLATION_TRENCH"] = (11, 0)
 config.GLOBAL["LAYERS"]["CONTACT_DOPING"] = (12, 0)
 config.GLOBAL["LAYERS"]["METALIZATION"] = (13, 0)
 config.GLOBAL["LAYERS"]["PASSIVATION_OPEN"] = (14, 0)
+config.GLOBAL["LAYERS"]["ISOLATION_TRENCH"] = (15, 0)
 
 config.GLOBAL["LAYERS"]["MASK_LABEL"] =(62, 3)
 config.GLOBAL["LAYERS"]["MASK_NAME"] =(63, 3)
@@ -33,6 +33,7 @@ config.GLOBAL["LAYERS"]["EXPORT_CONTACT_DOPING"] = (112, 0)
 config.GLOBAL["LAYERS"]["EXPORT_METALIZATION"] = (113, 0)
 config.GLOBAL["LAYERS"]["EXPORT_PASSIVATION_OPEN"] = (114, 0)
 config.GLOBAL["LAYERS"]["EXPORT_DICING"] = (115, 0)
+config.GLOBAL["LAYERS"]["EXPORT_DICING"] = (116, 0)
 
 
 
@@ -45,6 +46,7 @@ config.GLOBAL["EXPORT"].add("EXPORT_METALIZATION", config.GLOBAL["LAYERS"]["MASK
 config.GLOBAL["EXPORT"].add("EXPORT_PASSIVATION_OPEN", config.GLOBAL["LAYERS"]["PASSIVATION_OPEN"])
 config.GLOBAL["EXPORT"].add("EXPORT_DICING", config.GLOBAL["LAYERS"]["DICING"])
 config.GLOBAL["EXPORT"].add("EXPORT_DICING", config.GLOBAL["LAYERS"]["WAFER_BORDER"])
+config.GLOBAL["EXPORT"].add("EXPORT_TRENCH", config.GLOBAL["LAYERS"]["ISOLATION_TRENCH"])
 
 config.GLOBAL["EXPORT"].always(config.GLOBAL["LAYERS"]["MASK_LABEL"])
 config.GLOBAL["EXPORT"].always(config.GLOBAL["LAYERS"]["MASK_NAME"])
@@ -58,13 +60,14 @@ def createMarkers(lib):
     field = MarkerField(lib, 'MARKER_FIELD', [
         ('1', 'ALIGNMENT_MARKS', 'CONTACT_DOPING', 20, False, True),
         ('2', 'ALIGNMENT_MARKS', 'METALIZATION', 20, False, False),
+        ('3', 'ALIGNMENT_MARKS', 'ISOLATION_TRENCH', 20, False, False),
     ])
 
 
-    marker_left = gdspy.CellReference(field.cell, origin=(-40500, 0))
+    marker_left = gdspy.CellReference(field.cell, origin=(-42000, 0))
     markers.add(marker_left)
 
-    marker_right = gdspy.CellReference(field.cell, origin=(+40500, 0), x_reflection=True, rotation=180)
+    marker_right = gdspy.CellReference(field.cell, origin=(+42000, 0), x_reflection=True, rotation=180)
     markers.add(marker_right)
 
     keepouts = [marker_left.get_bounding_box(), marker_right.get_bounding_box()]
